42deb46b2ce8ee0b395744b9859d0a6859d2a08d
Ivan Tovar <ivxn2309@gmail.com>
2016-02-23 14:00:06 -0600

    Fixed problems on load components

--- project/js/src/script.js
+++ project/js/src/script.js
@@ -3,29 +3,32 @@ Author: Ivan Tovar (Webmaster at S+E)
 e-mail: ivxn2309@hotmail.com
   Date: Feb 05, 2016
 */
+$( document ).ready(function(){
 
-new navplate({
-    selector: '#nav-trigger',
-    type: 'slide',
-    active: 'always',
-    reveal: 'left'
-});
+    new navplate({
+        selector: '#nav-trigger',
+        type: 'slide',
+        active: 'always',
+        reveal: 'left'
+    });
 
-new flickerplate({
-    selector: '#mainbanner',
-    dots: true,
-    arrows: false,
-    animation: 'transform-slide'
-});
+    new flickerplate({
+        selector: '#mainbanner',
+        dots: true,
+        arrows: false,
+        animation: 'transform-slide'
+    });
 
-new flickerplate({
-    selector: '#portfolio',
-    dots: false,
-    arrows: true,
-    animation: 'transition-slide',
-    autoFlick: false
-});
+    new flickerplate({
+        selector: '#portfolio',
+        dots: false,
+        arrows: true,
+        animation: 'transition-slide',
+        autoFlick: false
+    });
 
-new formplate({
-    selector: '.formplate'
+    new formplate({
+        selector: '.formplate'
+    });
+
 });
